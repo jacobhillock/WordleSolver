79bc4392b70a894f8ef3dd9c92787fd7fc7cf057
Jacob Hillock <jacob@hillock.cc>
2022-05-17 20:19:05 -0400

    enhance processing to sense count of letters in word

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from operator import ge
 from sys import exit
 from argparse import ArgumentParser, Namespace
-from re import search
+from re import search, findall
 
 # Custom Imports
 from Custom_Exceptions import LetterException, LoadException, FormatException
@@ -23,9 +23,15 @@ def get_args() -> Namespace:
     return args
 
 
-def get_allowed_words(bank: list[str], regex: str, include: list[str], exclude: list[str]):
+def include_word(word: str, char: str, count: int) -> bool:
+    finds = findall(char, word)
+    return len(finds) == count
+
+
+def get_allowed_words(bank: list[str], regex: str, include: dict[str, int], exclude: list[str]):
     bank = [w for w in bank if search(regex, w)]
-    bank = [w for w in bank if all([char in w for char in include])]
+    bank = [w for w in bank if all(
+        [include_word(w, char, count) for char, count in include.items()])]
     bank = [w for w in bank if all([char not in w for char in exclude])]
 
     return bank
@@ -34,7 +40,7 @@ def get_allowed_words(bank: list[str], regex: str, include: list[str], exclude: 
 def main():
     args = get_args()
     regex = ""
-    include = []
+    include = {}
     exclude = []
     try:
         words = load()
--- process_words.py
+++ process_words.py
@@ -1,17 +1,18 @@
 from re import findall
 from Custom_Exceptions import LetterException, FormatException
 from globals import WORD_LENGTH
+from typing import Tuple
 
 
-def process_guessed_words(words_guessed: list[str]):
-    include = []
+def process_guessed_words(words_guessed: list[str]) -> Tuple[str, dict[str, int], list[str]]:
+    include = {}
     exclude = []
     regex_list = [list([]) for _ in range(WORD_LENGTH)]
 
     # process words
     for word in words_guessed:
         word_t = word
-
+        run_include = {}
         # find letters in word
         letters = findall("\w", word)
 
@@ -26,20 +27,20 @@ def process_guessed_words(words_guessed: list[str]):
                 # ensure letter was not solved
                 if type(regex_list[letterPos]) == list:
                     regex_list[letterPos].append(letter)
-                if letter not in include:
+                if letter not in run_include.keys():
                     exclude.append(letter)
 
                 word_t = word_t[1:]
 
             # letter is surrounded by [] meaning it is correct
             elif word_t[i-1] == '[' and word_t[i+1] == ']':
-                include.append(letter)
+                run_include[letter] = run_include.get(letter, 0) + 1
                 regex_list[letterPos] = letter
                 word_t = word_t[3:]
 
             # letter is surrounded by {} meaning it is in word
             elif word_t[i-1] == '{' and word_t[i+1] == '}':
-                include.append(letter)
+                run_include[letter] = run_include.get(letter, 0) + 1
                 if (type(regex_list[letterPos]) == list):
                     regex_list[letterPos].append(letter)
                 word_t = word_t[3:]
@@ -52,12 +53,14 @@ def process_guessed_words(words_guessed: list[str]):
                 - Incorrect Location: {{a}}
                 - Not in word: a
                 """)
+        for key, count in run_include.items():
+            include[key] = count if count > include.get(
+                key, 0) else include.get(key, 0)
 
     # sanitize data
     for i, reg in enumerate(regex_list):
         if type(reg) == list:
             regex_list[i] = [value for value in reg if value not in exclude]
-    include = list(set(include))
     exclude = [e for e in exclude if e not in include]
     exclude = list(set(exclude))
 
